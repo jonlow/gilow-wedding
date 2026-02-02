1a54cdacf6b5fc29d6036acc032537684ae0d186
Jon Low <jon@avenue.studio>
2026-02-02 11:53:52 +1100

    feat: configure image caching settings in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    minimumCacheTTL: 15552000, // 6 months in seconds
+  },
 };
 
 export default nextConfig;
